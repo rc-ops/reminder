d6f500fad183ef36812914f0f3c6fc95f3402ef5
rc-ops <rodrigocarvalhocontato@protonmail.com>
2022-08-18 17:33:29 -0300

    First test build

--- reminder/Properties/AssemblyInfo.cs
+++ reminder/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Reminder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Reminder Timer")]
+[assembly: AssemblyDescription("A simple reminder and timer written in C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Reminder")]
